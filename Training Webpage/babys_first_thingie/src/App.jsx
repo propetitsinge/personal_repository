49deff5a6b875fa08566e644cf3fa858d141c696
Pierre Munger <propetitsinge@gmail.com>
2025-09-29 17:11:07 -0700

    I made a thing

---
+++ Training Webpage/babys_first_thingie/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+
+function App() {
+  const [temperatureValue, setTemperatureValue] = useState(72);
+  const [temperatureColor, setTemperatureColor] = useState('neutral');
+
+  const increaseTemp = () => {
+    if (temperatureValue === 100) return;
+
+    const newTemp = temperatureValue + 1;
+
+    if(newTemp >= 80){
+      setTemperatureColor('hot');
+    }
+    else if(newTemp < 80 && newTemp >= 68){
+      setTemperatureColor('neutral');
+    }
+    else{
+      setTemperatureColor('cold');
+    }
+
+    setTemperatureValue(newTemp);
+  }
+
+  const decreaseTemp = () => {
+    if (temperatureValue === 62) return;
+    
+    const newTemp = temperatureValue - 1;
+
+    if(newTemp <= 68){
+      setTemperatureColor('cold');
+    }
+    else if(newTemp < 80 && newTemp >= 68){
+      setTemperatureColor('neutral');
+    }
+    else{
+      setTemperatureColor('hot');
+    }
+
+    setTemperatureValue(newTemp);
+  }
+
+  return (
+    <div>  
+      <div className="app-container">
+        <div className="temperature-display-container">
+          <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°F</div>
+        </div>
+        <div className="button-container">
+          <button onClick={() => decreaseTemp()}>-</button>
+          <button onClick={() => increaseTemp()}>+</button>
+        </div>
+      </div>
+    </div>
+  ) 
+}
+
+export default App
